273fe33b17748c4f91a6b8e17814b4311e096ed1
rohitsingh2077 <robertrohit28@gmail.com>
2025-12-09 17:42:22 +0530

    final commit before deployment

--- backend/index.js
+++ backend/index.js
@@ -5,13 +5,15 @@ dotenv.config(); // can access the dotenv from anywhere
 const cookieParser = require("cookie-parser");
 
 const {app,server} = require('./Socket/socket.js');
+const path =require("path");
+const __dirname = path.resolve();
 
 //external modules
 const authRouter = require('./routes/authRouter');
 const messageRouter = require('./routes/messsageRoute')
 const userRouter =require('./routes/userRouter')
 const updateRouter = require('./routes/updateRouter.js');
-const friendRouter = require('./routes/friendRoutes.js');
+// const friendRouter = require('./routes/friendRoutes.js');
 
 app.use(cookieParser());
 
@@ -23,7 +25,14 @@ app.use('/api/auth',authRouter);
 app.use('/api/message',messageRouter);
 app.use('/api/user',userRouter);
 app.use('/api/update',updateRouter);
-app.use('/api/friends',friendRouter);
+// app.use('/api/friends',friendRouter);
+
+app.use(express.static(path.join(__dirname,'/frontend/dist')));
+
+app.get('*',(req,res)=>{
+  res.sendFile(path.join(__dirname,"frontend","dist","index.html"));
+})
+
 PORT = process.env.PORT || 3001;
 
 // Connect to DB first, then start server
