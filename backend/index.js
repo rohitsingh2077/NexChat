5c73024f9a264427618a6a11c0c23b7159c85348
rohitsingh2077 <robertrohit28@gmail.com>
2025-12-09 20:00:24 +0530

    3

--- backend/index.js
+++ backend/index.js
@@ -29,7 +29,7 @@ app.use('/api/update',updateRouter);
 
 app.use(express.static(path.join(root,'/frontend/dist')));
 
-app.get('*',(req,res)=>{
+app.get('/*',(req,res)=>{
   res.sendFile(path.join(root,"frontend","dist","index.html"));
 })
 
